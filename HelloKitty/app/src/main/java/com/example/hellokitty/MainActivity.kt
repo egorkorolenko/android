7810b61f95a099cb34bb2333baa733a5781703de
Egor Korolenko <korolenko.egor39@gmail.com>
2021-10-17 18:37:22 +0600

    Первое задание

---
+++ HelloKitty/app/src/main/java/com/example/hellokitty/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.hellokitty
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    private var mHelloTextView: TextView? = null
+    private var mNameEditText: EditText? = null
+    private var button: ImageButton? = null;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        mHelloTextView = findViewById(R.id.textView)
+        mNameEditText = findViewById(R.id.editTextTextPersonName)
+        button = findViewById(R.id.imageButton)
+
+        button?.setOnClickListener{
+            onClick()
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun onClick() {
+        if (mNameEditText == null) {
+            mHelloTextView!!.text = "Hello Kitty!"
+        } else {
+            mHelloTextView!!.text = "Hi," + mNameEditText!!.text;
+        }
+    }
+}
