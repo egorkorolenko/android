9823e4ba279e69039aacc1d2ba13bf6cd6045793
Egor Korolenko <korolenko.egor39@gmail.com>
2021-10-19 21:06:28 +0600

    начал делать 5 проект

---
+++ SwitchingBetweenApplicationScreens/app/src/main/java/com/example/switchingbetweenapplicationscreens/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.switchingbetweenapplicationscreens
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button: Button = findViewById(R.id.button)
+
+        button.setOnClickListener {
+            val intent = Intent(this@MainActivity, AboutActivity::class.java)
+            startActivity(intent)
+        }
+
+        val button2: Button = findViewById(R.id.button2)
+
+        button2.setOnClickListener {
+            val intent = Intent(this@MainActivity, SecondActivity::class.java)
+            startActivity(intent)
+        }
+
+        val send:Button = findViewById(R.id.send)
+
+        send.setOnClickListener{
+            val intent = Intent(this@MainActivity, SecondActivity::class.java)
+            val username: EditText = findViewById(R.id.editUserName)
+            val gifts: EditText = findViewById(R.id.editGifts)
+            intent.putExtra("username",username.text.toString())
+            intent.putExtra("gift",gifts.text.toString())
+            startActivity(intent)
+        }
+    }
+}
---
+++ SwitchingBetweenApplicationScreens/app/src/main/java/com/example/switchingbetweenapplicationscreens/SecondActivity.kt
@@ -0,0 +1,21 @@
+package com.example.switchingbetweenapplicationscreens
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class SecondActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second2)
+        val text: TextView = findViewById(R.id.mess)
+
+        val user = intent.extras?.getString("username")
+        val gifts = intent.extras?.getString("gift")
+
+        text.text = user +"передал вам" + gifts
+
+    }
+}
