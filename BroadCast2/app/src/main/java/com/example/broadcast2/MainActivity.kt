8d186aea31513757e51d8f5f589a90e76c207813
Egor Korolenko <korolenko.egor39@gmail.com>
2021-11-20 14:28:09 +0600

    проект 29. Broadcast

---
+++ BroadCast2/app/src/main/java/com/example/broadcast2/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.example.broadcast2
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import java.text.SimpleDateFormat
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val tickReceiver by lazy { makeBroadcastReceiver() }
+
+    companion object {
+        private fun getCurrentTimeStamp(): String {
+            val simpleDateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.US)
+            val now = Date()
+            return simpleDateFormat.format(now)
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onResume() {
+        val dateTimeTextView: TextView = findViewById(R.id.text)
+        super.onResume()
+        dateTimeTextView.text = getCurrentTimeStamp()
+        registerReceiver(tickReceiver, IntentFilter(Intent.ACTION_TIME_TICK))
+    }
+
+    override fun onPause() {
+        super.onPause()
+        try {
+            unregisterReceiver(tickReceiver)
+        } catch (e: IllegalArgumentException) {
+            Log.e("Broadcast", "Time tick Receiver not registered", e)
+        }
+    }
+
+
+    private fun makeBroadcastReceiver(): BroadcastReceiver {
+        val dateTimeTextView: TextView = findViewById(R.id.text)
+        return object : BroadcastReceiver() {
+            override fun onReceive(context: Context, intent: Intent?) {
+                if (intent?.action == Intent.ACTION_TIME_TICK) {
+                    dateTimeTextView.text = getCurrentTimeStamp()
+                }
+            }
+        }
+    }
+}
