bb9fda9b1d057d928883cdb8c9a897b1526ee9f2
Egor Korolenko <korolenko.egor39@gmail.com>
2022-02-18 13:22:40 +0600

    Коммит старой проги, чтобы она мне не мешалась

--- Wemonitorthepowerofthephone/app/src/main/java/com/example/wemonitorthepowerofthephone/MessageReceiver.kt
+++ Wemonitorthepowerofthephone/app/src/main/java/com/example/wemonitorthepowerofthephone/MessageReceiver.kt
@@ -1,4 +1,15 @@
 package com.example.wemonitorthepowerofthephone
 
-class MessageReceiver {
-}
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+
+class MessageReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        if (intent?.action.equals("android.intent.action.ACTION_POWER_DISCONNECTED")) {
+            val message = "Обнаружено сообщение "+ (intent?.action ?: String())
+                Toast.makeText(context, message, Toast.LENGTH_LONG).show()
+            }
+        }
+    }
